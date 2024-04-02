bb0d325b99e21720841b0b424ebe46692cffbc06
pedrocjx <pedro.ferreira2505@outlook.com>
2024-04-02 19:02:16 -0300

    sobre/rodape

--- codigo-fonte/vintage_web/src/App.js
+++ codigo-fonte/vintage_web/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Cabecalho from './pages/cabecalho';
 import Rodape from './pages/rodape';
-import Login from './pages/login'
+import Login from './pages/login';
+import Sobre from './pages/sobre';
 
 function App() {
   return (
     <div>
       
-     <Login/>
-      
+     <Cabecalho/>
+     <Sobre/>
+     <Rodape/>
     </div>
   );
 }
--- codigo-fonte/vintage_web/src/pages/rodape.js
+++ codigo-fonte/vintage_web/src/pages/rodape.js
@@ -2,9 +2,44 @@ import React from 'react';
 
 const Rodape = () => {
   return (
-    <footer>
-      {/* Adicione aqui qualquer conteúdo do rodapé */}
-      <p>&copy; 2024 Minha Empresa</p>
+    <footer className="rodapeTotal" >
+
+      <div className="rodapeConteudo">
+        <img className='rodapeLogo' src="/imagens/logo.png" alt="logomarca vintage barbearia" />
+      </div>
+
+      <div className="rodapeTexto1">
+
+        <p> Agendamentos <br></br> Quem somos <br></br> Contato <br></br> Serviços <br></br> </p>
+
+      </div>
+
+      <div className="rodapeTexto2">
+
+        <p> Acompanhe nossas <br></br> redes Sociais</p>
+
+        <img className='rodapeIcon' src="/imagens/insta-icon.png" alt="instagram" />
+        <img className='rodapeIcon' src="/imagens/ytb-icon.png" alt="youtube" />
+        <img className='rodapeIcon' src="/imagens/face-icon.png" alt="facebook" />
+
+      </div>
+
+      <div className="rodapeTexto3">
+
+        <h1 className='rodapeTitulo'> Endereço </h1>
+        <p> Rua Joaquim Teixeira dos Anjos 299<br></br> Bairro Taquaril<br></br> Belo Horizonte-MG </p>
+
+      </div>
+
+      <div className="rodapeTexto4">
+
+        <h1 className='rodapeTitulo2'> Aceitamos </h1>
+
+        <img className='rodapeIcon2' src="/imagens/cartao-icon.png" alt="cartao" />
+        <img className='rodapeIcon3' src="/imagens/pix-icon.png" alt="pix" />
+
+      </div>
+
     </footer>
   );
 };
