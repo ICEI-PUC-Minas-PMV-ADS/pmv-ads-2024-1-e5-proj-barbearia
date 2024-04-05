a8e674d5d906dd18e28d5fa3457335d7a9803e74
Guilherme C Gomes <guilermeextreme@gmail.com>
2024-04-05 00:22:25 -0300

    Tela de contato

--- codigo-fonte/vintage_web/src/App.js
+++ codigo-fonte/vintage_web/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import Cabecalho from './pages/cabecalho';
-import Rodape from './pages/rodape';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/login';
 import Sobre from './pages/sobre';
+import Contato from './pages/contato';
 
 function App() {
   return (
-    <div>
-      
-     <Login/>
-     
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/sobre" element={<Sobre />} />
+          <Route path="/contato" element={<Contato />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ codigo-fonte/vintage_web/src/pages/contato.js
@@ -0,0 +1,112 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import Cabecalho from './cabecalho';
+import Rodape from './rodape';
+import '../sobre.css';
+import apiUrl from '../services/ApiConfig';
+import Swal from 'sweetalert2';
+
+const Contato = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    telephone: '',
+    email: '',
+    message: ''
+  });
+
+  const [responseMessage, setResponseMessage] = useState('');
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(`${apiUrl}/contact-us`, formData);
+      Swal.fire({
+        position: "top-center",
+        icon: "success",
+        title: "Sucesso!",
+        showConfirmButton: false,
+        timer: 1500
+      });
+    } catch (error) {
+      Swal.fire({
+        title: 'Erro',
+        text: error.response.data.message,
+        icon: 'error',
+        confirmButtonText: 'Fechar'
+      });
+      console.error('Erro ao enviar mensagem:', error);
+    }
+  };
+
+  return (
+    <div>
+      <Cabecalho />
+      <div id="contact-us">
+        <h2>Fale Conosco</h2>
+      </div>
+      <div id="content">
+        <h2 id="msg">Envie uma mensagem</h2>
+        <div id="form">
+          <form className="form-container" onSubmit={handleSubmit}>
+            <div className="input-group">
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                required
+                className="input-field"
+                placeholder="Nome"
+              />
+            </div>
+            <div className="input-group">
+              <input
+                type="tel"
+                id="phone"
+                name="telephone"
+                value={formData.phone}
+                onChange={handleChange}
+                required
+                className="input-field"
+                placeholder="Telefone"
+              />
+            </div>
+            <div className="input-group">
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+                className="input-field"
+                placeholder="Email"
+              />
+            </div>
+            <div className="input-group">
+              <textarea
+                id="message"
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                required
+                className="textarea-field"
+                placeholder="Comentario"
+              ></textarea>
+            </div>
+            <button type="submit" className="submit-button">Enviar</button>
+          </form>
+          {responseMessage && <p>{responseMessage}</p>}
+        </div>
+      </div>
+      <Rodape />
+    </div>
+  );
+};
+
+export default Contato;
